ff57a35a6d3a279d36ea34e9e3993eddf7d97f28
longnguyen29102000 <long.nguyen@siliconstack.com.au>
2023-09-25 10:28:47 +0700

    Revert "update code"
    This reverts commit 1c7daafb360c3079676150480fd611bfd4d9f2ed.

---
+++ Shared.Repository.MongoDb/Data/Interfaces/IMongoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Shared.Repository.Configuration.Settings;
+
+namespace Shared.Repository.MongoDb.Data.Interfaces;
+public interface IMongoContext : IDisposable
+{
+    void AddCommand(Func<Task> func);
+    Task<int> SaveChanges();
+    IMongoCollection<T> GetCollection<T>(string name);
+}
---
+++ Shared.Repository.MongoDb/Domains/Interfaces/IEntityBase.cs
@@ -0,0 +1,5 @@
+﻿namespace Shared.Repository.MongoDb.Domains.Interfaces;
+public interface IEntityBase<T>
+{
+    T Id { get; set; }
+}
